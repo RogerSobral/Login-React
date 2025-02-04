4ba5cace60a97fbba22b610253cbbaf51b73b58b
Rogério Sobral Ribeiro <sobralcomix@gmail.com>
2025-02-04 13:35:05 -0300

    Mudar o Date
    Mudar a cor dos dias do calendário

--- src/components/DateInput.jsx
+++ src/components/DateInput.jsx
@@ -1,8 +1,15 @@
+// npm install date-fns
+
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css'; // Importa os estilos do DatePicker
 import 'font-awesome/css/font-awesome.min.css'; // Importa o CSS do Font Awesome
+import { ptBR } from 'date-fns/locale'; // Importa a localização em português
+import { registerLocale } from 'react-datepicker'; // Importa a função para registrar a localização
 import './DateInput.css'; // Certifique-se de criar um arquivo CSS para os estilos
+
+// Registra a localização em português
+registerLocale('pt-BR', ptBR);
 
 const DateInput = () => {
     const [date, setDate] = useState(null); // Inicializa o estado da data
@@ -15,6 +22,7 @@ const DateInput = () => {
                 placeholderText="Selecione uma data"
                 className="custom-date-input"
                 dateFormat="dd/MM/yyyy" // Formato da data
+                locale="pt-BR" // Define a localização para português
             />
             <i className="fa fa-calendar calendar-icon"></i> {/* Ícone de calendário */}
         </div>
